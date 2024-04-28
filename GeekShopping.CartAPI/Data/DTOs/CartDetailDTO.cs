7fb98b9ab061a53e1c280a7068c1d50fed2ac906
larislav <laarissa_barros@hotmail.com>
2024-04-28 11:51:02 -0300

    Entidades e repositorio

---
+++ GeekShopping.CartAPI/Data/DTOs/CartDetailDTO.cs
@@ -0,0 +1,18 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CartAPI.Data.DTOs
+{
+    public class CartDetailDTO 
+    {
+        public long Id { get; set; }
+        public long CartHeaderId { get; set; }
+        public CartHeaderDTO CartHeader { get; set; }
+
+        public long ProductId { get; set; }
+        public ProductDTO Product { get; set; }
+
+        public int Count { get; set; }
+
+    }
+}
---
+++ GeekShopping.CartAPI/Model/CartDetail.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CartAPI.Model
+{
+    [Table("cart_detail")]
+    public class CartDetail : BaseEntity
+    {
+        [ForeignKey("cart_header_id")]
+        public long CartHeaderId { get; set; }
+        public virtual CartHeader CartHeader { get; set; }
+
+        [ForeignKey("product_id")]
+        public long ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        [Column("count")]
+        public int Count { get; set; }
+
+    }
+}
